7ececefb4582bdc16d737eeffeb885beaa8994cb
Mr-Rain <17766782253@163.com>
2025-04-30 18:02:38 +0800

    功能：更新API代理目标地址，优化网络请求配置

--- vue-front/vite.config.ts
+++ vue-front/vite.config.ts
@@ -144,7 +144,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8080',
+        target: 'https://119.84.246.217:18341',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path
